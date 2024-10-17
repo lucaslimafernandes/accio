4dd492079545cec064d23661ebd00c0152bec813
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2024-10-16 23:21:44 -0300

    Chore: main first draft

--- cmd/accio/main.go
+++ cmd/accio/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	_help := flag.Bool("help", false, "Show available commands")
+
+	flag.Parse()
+
+	if *_help {
+		flag.PrintDefaults()
+	}
+
+	switch os.Args[1] {
+	case "run":
+		runnn()
+	default:
+		fmt.Printf("Unknown subcommand: %s\n", os.Args[1])
+	}
+
+	if len(os.Args) == 1 {
+		fmt.Println("Maybe you forget some commands, use '-help' to see available commands.")
+		return
+	}
+}
+
+func runnn() {
+	fmt.Println("Executing...")
+}
