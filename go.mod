d17a76cb8bb4a090c72a37117d8a24eb1f6f36e7
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2024-10-19 14:00:00 -0300

    func ReadRunfile

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.5
 require (
 	github.com/BurntSushi/toml v1.4.0
 	golang.org/x/crypto v0.28.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require golang.org/x/sys v0.26.0 // indirect
--- pkg/read_files/runfile.go
+++ pkg/read_files/runfile.go
@@ -1,5 +1,12 @@
 package readfiles
 
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 type Task struct {
 	Name    string   `yaml:"name"`
 	Node    []string `yaml:"node"`
@@ -10,4 +17,22 @@ type Runfile struct {
 	Name       string `yaml:"name"`
 	RemoteUser string `yaml:"remote_user"`
 	Tasks      []Task `yaml:"tasks"`
+}
+
+func ReadRunfile(path *string) (*Runfile, error) {
+
+	var runfile Runfile
+
+	f, err := os.ReadFile(*path)
+	if err != nil {
+		return &runfile, fmt.Errorf("unable to read runfile: %v", err)
+	}
+
+	err = yaml.Unmarshal(f, &runfile)
+	if err != nil {
+		return &runfile, fmt.Errorf("unable to parse runfile, please verify file: %v", err)
+	}
+
+	return &runfile, nil
+
 }
