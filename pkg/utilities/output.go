4fea280f907d6ccf5adc3bcb7ae9f4cedebde982
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2024-12-01 15:00:36 -0300

    ExtractVarsReturn

--- pkg/utilities/output.go
+++ pkg/utilities/output.go
@@ -1,6 +1,7 @@
 package utilities
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -21,7 +22,7 @@ func SaveOutputAsFile(filepath, text *string) {
 
 }
 
-func ExtractVars(text string) string {
+func ExtractVars(text string, vars map[string]string) string {
 
 	start := strings.Index(text, "{{") + 2
 	end := strings.Index(text, "}}")
@@ -33,4 +34,51 @@ func ExtractVars(text string) string {
 	}
 
 	return ""
+}
+
+func ExtractVarsReturn(text string, vars map[string]string) string {
+
+	var result string
+	start := 0
+
+	for {
+		// Encontrar as posições de {{ e }}
+		s := strings.Index(text[start:], "{{")
+		if s == -1 {
+			// Se não encontrar mais, acrescenta o restante da string
+			result += text[start:]
+			break
+		}
+
+		// Adiciona o texto anterior ao resultado
+		result += text[start : start+s]
+
+		// Atualiza o índice de início para a próxima iteração
+		start += s + 2
+		e := strings.Index(text[start:], "}}")
+		if e == -1 {
+			// Caso não encontre o fechamento, interrompe
+			break
+		}
+
+		// Extrai o nome da variável entre as chaves
+		variable := strings.TrimSpace(text[start : start+e])
+
+		// Substitui a variável pelo valor no mapa
+		val, ok := vars[variable]
+		if ok {
+			result += val
+		} else {
+			// Se não encontrar a chave, mantém o texto original
+			result += "{{" + variable + "}}"
+		}
+
+		// Atualiza o índice de início para o próximo bloco de texto
+		start += e + 2
+	}
+
+	// Exibe o resultado final
+	fmt.Println(result)
+
+	return result
 }
